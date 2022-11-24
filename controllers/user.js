1df392763a5ac13d4dd1e374ee4ce1865da1ed61
stiven4950 <omarrocha4950@gmail.com>
2022-11-24 08:08:12 -0500

    Primer commit

---
+++ controllers/user.js
@@ -0,0 +1,51 @@
+const { response, request } = require('express');
+
+const usuariosGet = (req = request, res = response) => {
+    const { apikey, name, q } = req.query;
+
+    res.json({
+        msg: "get API - controlador",
+        apikey,
+        name,
+        q
+    });
+}
+
+const usuariosPost = (req, res = response) => {
+    const body = req.body;
+
+
+    res.json({
+        msg: "post API - controlador",
+        body
+    });
+}
+
+const usuariosPut = (req, res = response) => {
+    const id = req.params.id;
+
+    res.json({
+        msg: "put API - controlador",
+        id,
+    });
+}
+
+const usuariosPatch = (req, res = response) => {
+    res.json({
+        msg: "patch API - controlador",
+    });
+}
+
+const usuariosDelete = (req, res = response) => {
+    res.json({
+        msg: "delete API - controlador",
+    });
+}
+
+module.exports = {
+    usuariosGet,
+    usuariosPost,
+    usuariosPut,
+    usuariosPatch,
+    usuariosDelete,
+}
---
+++ models/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+
+class Server {
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+        this.usuariosPath = "/api/usuarios"
+
+        this.middlewares();
+
+        this.routes();
+    }
+
+    middlewares() {
+        this.app.use(express.static("public"));
+        this.app.use(cors());
+        this.app.use(express.json());
+    }
+
+    routes() {
+        this.app.use(this.usuariosPath, require("../routes/user"));
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log("Servidor corriendo en el puerto ", this.port);
+        });
+    }
+}
+
+
+module.exports = Server;
